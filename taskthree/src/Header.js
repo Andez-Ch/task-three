564a5e88c2d16ff8e0fa28bf7ff6ef0c8bb068ff
Andez-Ch <chaiga36@gmail.com>
2024-06-27 12:32:01 +0300

    added header.css
    it's for styling the header

--- taskthree/src/Header.js
+++ taskthree/src/Header.js
@@ -1,18 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './Header.css';
 
 export const Header = () => {
   return (
-    <div>
-      <Link to="/">Home</Link> &nbsp;&nbsp;&nbsp;
-      <Link to="/FunctionalComp">Functional component</Link>&nbsp;&nbsp;&nbsp;
-      <Link to="/class_comp">Class component</Link>&nbsp;&nbsp;&nbsp;
-      <Link to="/FirstJsxSyntax">First jsx component</Link>&nbsp;&nbsp;&nbsp;
-      <Link to="/SecondJsxSyntax">Second Jsx component</Link>&nbsp;&nbsp;&nbsp;
-      <Link to="/ThirdJsxSyntax">Third Jsx component</Link>&nbsp;&nbsp;&nbsp;
-      <Link to="/Greeting">First Prop syntax</Link>&nbsp;&nbsp;&nbsp;
-      <Link to="/Headz">Second Prop syntax</Link>&nbsp;&nbsp;&nbsp;
-      <Link to="/AndezCh">State syntax</Link>&nbsp;&nbsp;&nbsp;
+    <div class="header-container">
+      <Link class="header-link" to="/">Home</Link> &nbsp;&nbsp;&nbsp;
+      <Link class="header-link" to="/FunctionalComp">Functional component</Link>&nbsp;&nbsp;&nbsp;
+      <Link class="header-link" to="/class_comp">Class component</Link>&nbsp;&nbsp;&nbsp;
+      <Link class="header-link" to="/FirstJsxSyntax">First jsx component</Link>&nbsp;&nbsp;&nbsp;
+      <Link class="header-link" to="/SecondJsxSyntax">Second Jsx component</Link>&nbsp;&nbsp;&nbsp;
+      <Link class="header-link" to="/ThirdJsxSyntax">Third Jsx component</Link>&nbsp;&nbsp;&nbsp;
+      <Link class="header-link" to="/Greeting">First Prop syntax</Link>&nbsp;&nbsp;&nbsp;
+      <Link class="header-link" to="/Headz">Second Prop syntax</Link>&nbsp;&nbsp;&nbsp;
+      <Link class="header-link" to="/AndezCh">State syntax</Link>&nbsp;&nbsp;&nbsp;
 
     </div>
   );
